0cebd995ffca121c047655f4ba3df549a131d3d5
Simon Liebelt <sliebelt@eltec.de>
2020-07-27 12:57:42 +0200

    Update config

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (which-key ivy-posframe dictcc counsel swiper ivy dired-subtree google-this magit buffer-move solarized-theme use-package))))
+    (olivetti which-key ivy-posframe dictcc counsel swiper ivy dired-subtree google-this magit buffer-move solarized-theme use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
